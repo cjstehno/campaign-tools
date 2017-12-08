632e1a30eb54cfec5fbaa549a68482ea5941a503
cjstehno <chris@stehno.com>
2017-12-08 08:49:55 -0600

    initial commit

---
+++ src/main/kotlin/com/stehno/dd/campaigntools/CampaignToolsApplication.kt
@@ -0,0 +1,11 @@
+package com.stehno.dd.campaigntools
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CampaignToolsApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(CampaignToolsApplication::class.java, *args)
+}
